49b568cb4152f56eb33ae669a3df4098439881e5
Tobi <noggnzzz@gmail.com>
2025-03-16 14:28:52 -0500

    abreviaturas

--- script.mjs
+++ script.mjs
@@ -1,15 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
   const schedule = [
-      { time: "08:00 - 09:30", activities: ["Razonamiento Matemático (2do Sec - Padua)", "Álgebra (2do Sec - Padua)", "Física (6to Prim - Padua)", "Álgebra (6to Prim - Padua)", "Física (4to Sec - Noveno)", ""] },
+      { time: "08:00 - 09:30", activities: ["RM (2°Sec - P)", "Álg (2°Sec - P)", "Fís (6°P - P)", "Álg (6°P - P)", "Fís (4°Sec - N)", ""] },
       { time: "09:30 - 09:45", isBreak: true },
-      { time: "09:45 - 11:15", activities: ["Razonamiento Matemático (1ro Sec - Padua)", "Física (2do Sec - Padua)", "Historia del Perú (2do Sec - Padua)", "Física (3ro Sec - Padua)", "Física (1ro Sec - Noveno)", ""] },
+      { time: "09:45 - 11:15", activities: ["RM (1°Sec - P)", "Fís (2°Sec - P)", "Hist (2°Sec - P)", "Fís (3°Sec - P)", "Fís (1°Sec - N)", ""] },
       { time: "11:15 - 11:30", isBreak: true },
-      { time: "11:30 - 13:00", activities: ["Álgebra (1ro Sec - Padua)", "Química (1ro Sec - Padua)", "Historia del Perú (3ro Sec - Padua)", "Cívica (2do Sec - Padua)", "Física (2do Sec - Noveno)", ""] },
-      { time: "13:00 - 14:30", activities: ["Razonamiento Matemático (3ro Sec - Padua)", "Álgebra (3ro Sec - Padua)", "Historia del Perú (1ro Sec - Padua)", "Física (1ro Sec - Padua)", "Física (3ro Sec - Noveno)", ""] },
-      { time: "15:30 - 17:00", activities: ["Ensayo Banda (Padua)", "", "", "", "Ensayo Banda (Noveno)", ""] },
-      { time: "18:30 - 20:00", activities: ["Estadística Inferencial (0303-B)", "Lab. Mecánica Clásica", "Cálculo 1", "Mecánica Clásica", "Estadística Inferencial (0303-B)", "Diseño de Productos y Servicios (0703-D)"] },
-      { time: "20:15 - 22:30", activities: ["", "Mecánica Clásica", "", "", "", ""] },
-      { time: "24/7", activities: ["Herramientas Informáticas (Virtual)", "Herramientas Informáticas (Virtual)", "Herramientas Informáticas (Virtual)", "Herramientas Informáticas (Virtual)", "Herramientas Informáticas (Virtual)", "Herramientas Informáticas (Virtual)"] }
+      { time: "11:30 - 13:00", activities: ["Álg (1°Sec - P)", "Quím (1°Sec - P)", "Hist (3°Sec - P)", "Cív (2°Sec - P)", "Fís (2°Sec - N)", ""] },
+      { time: "13:00 - 14:30", activities: ["RM (3°Sec - P)", "Álg (3°Sec - P)", "Hist (1°Sec - P)", "Fís (1°Sec - P)", "Fís (3°Sec - N)", ""] },
+      { time: "15:30 - 17:00", activities: ["Ensayo (P)", "", "", "", "Ensayo (N)", ""] },
+      { time: "18:30 - 20:00", activities: ["EstInf (0303-B)", "Lab MecClás", "Cál 1", "MecClás", "EstInf (0303-B)", "Diseño P&S (0703-D)"] },
+      { time: "20:15 - 22:30", activities: ["", "MecClás", "", "", "", ""] },
+      { time: "24/7", activities: ["Herr Inf (V)", "Herr Inf (V)", "Herr Inf (V)", "Herr Inf (V)", "Herr Inf (V)", "Herr Inf (V)"] }
   ];
 
   const tbody = document.querySelector("#schedule tbody");
