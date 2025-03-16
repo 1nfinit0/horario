a9c35b02e8393355bd8d3c4e5b4be7a092c197f3
Tobi <noggnzzz@gmail.com>
2025-03-16 14:38:06 -0500

    abreviaturas

--- script.mjs
+++ script.mjs
@@ -6,8 +6,9 @@ document.addEventListener("DOMContentLoaded", () => {
       { time: "11:15 - 11:30", isBreak: true },
       { time: "11:30 - 13:00", activities: ["Álg (1°Sec - P)", "Quím (1°Sec - P)", "Hist (3°Sec - P)", "Cív (2°Sec - P)", "Fís (2°Sec - N)", ""] },
       { time: "13:00 - 14:30", activities: ["RM (3°Sec - P)", "Álg (3°Sec - P)", "Hist (1°Sec - P)", "Fís (1°Sec - P)", "Fís (3°Sec - N)", ""] },
+      { time: "14:30 - 16:45", activities: ["", "", "", "", "", "Diseño P&S (0703-D)"] }, // Curso corregido
       { time: "15:30 - 17:00", activities: ["Ensayo (P)", "", "", "", "Ensayo (N)", ""] },
-      { time: "18:30 - 20:00", activities: ["EstInf (0303-B)", "Lab MecClás", "Cál 1", "MecClás", "EstInf (0303-B)", "Diseño P&S (0703-D)"] },
+      { time: "18:30 - 20:00", activities: ["EstInf (0303-B)", "Lab MecClás", "Cál 1", "MecClás", "EstInf (0303-B)", "Cál 1"] }, // Curso corregido
       { time: "20:15 - 22:30", activities: ["", "MecClás", "", "", "", ""] },
       { time: "24/7", activities: ["Herr Inf (V)", "Herr Inf (V)", "Herr Inf (V)", "Herr Inf (V)", "Herr Inf (V)", "Herr Inf (V)"] }
   ];
